6bd4de1dba485b2a1b5c662e23cb2a6113d7d847
joydotcom <msjoyjackson@gmail.com>
2021-03-29 23:05:13 -0400

    added queryUrl

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
 var searchForm = $("#search-form");
 var searchTermEl = $("#search-term");
 
-searchForm.on ("submit", function(event) {
-event.preventDefault()
+searchForm.on("submit", function (event) {
+  event.preventDefault();
 });
 
-var searchterm = searchTermEl.val();
+var searchTerm = searchTermEl.val();
 var longitude = "";
 var latitude = "";
 
-var apiKey = "6ac8192d5fafbf69b00f77373239cae0"
+var apiKey = "6ac8192d5fafbf69b00f77373239cae0";
+
+var queryUrl =
+  "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+  latitde +
+  "&lon=" +
+  longitude +
+  "&exclude={part}&appid=" +
+  apiKey;
+
+  
