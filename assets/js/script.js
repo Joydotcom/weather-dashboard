402445b2f9687e988e268341cfea1c813310bfb2
joydotcom <msjoyjackson@gmail.com>
2021-03-29 22:50:36 -0400

    added longitude and lat variables as placeholders

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 var searchForm = $("#search-form");
 var searchTermEl = $("#search-term");
 
+searchForm.on ("submit", function(event) {
+event.preventDefault()
+});
+
+var searchterm = searchTermEl.val();
+var longitude = "";
+var latitude = "";
