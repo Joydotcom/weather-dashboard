15fcf70d32914c8fe513f5200bb9f92acb63a442
joydotcom <msjoyjackson@gmail.com>
2021-04-05 22:07:11 -0400

    added for loop

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -9,10 +9,12 @@ window.addEventListener("load", function() {
 
 })
 
+var searchTermEl = $("#search-term");
 var apiKey = "6ac8192d5fafbf69b00f77373239cae0";
 var historyItems = [];
-
 var searchForm = $("#search-form");
+
+
 
 searchForm.on("submit", function (event) {
     event.preventDefault();
@@ -31,12 +33,54 @@ searchForm.on("submit", function (event) {
         return response.json();
       })
       .then(function (data) {
-       
-        getForecast(searchTerm);
+       getForecast(searchTerm);
         getUV(data.coord.lat, data.coord.lon);
   
       });
   });
 
 
+var forecastEl = document.querySelector("#forecast")
 
+forecastEl.innerHTML= '<h4 class="mt-3">'
+
+forecastRowEl = document.createElement('div');
+forecastRowEl.className = '"row"';
+
+console.log(data.list.length);
+
+
+function getForecast(searchTerm){
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q="+searchTerm+"&appid="+apiKey+"&units=imperial")
+    .then(function (response) {
+        return response.json();
+        })
+      .then(function (data) {
+          console.log(data.list.length);
+    
+
+for (var i = 0; i < (data.list.length); i++) {
+if (data.list[i].dt.txt.index('15:00:00') !== -1){
+var cardCol = document.createElement('div');
+cardCol.classList.add('col-md-2');
+var cardEl = document.createElement('div');
+cardEl.classList.add('card','text-white','bg-primary');
+var windSpeedEl = document.createElement ('p');
+windSpeedEl.classList.add('card-text');
+var humEl = document.createElement('p');
+humEl.classList.add('card-text');
+var bodyEl = document.createElement('div');
+bodyEl.classList.add('card-body','p-2');
+var titleEl = document.createElement('h5')
+titleEl.classList.add('card-title');
+titleEl.textContent = new Date (
+    data.list[i].dt.txt
+) .toLocaleDateString();
+var iconEl = document.createElement('img');
+iconEl.setAttribute(
+    'src' , "https://openweathermap.org/img/w/"+ data.list[i].weather[0].icon +".png"
+)
+}
+}
+})
+}
